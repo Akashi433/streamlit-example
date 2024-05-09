9435450da60a10849a0227fa742a3b460362d28f
Akashi433 <tohkarinka464@gmail.com>
2024-05-09 07:45:39 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,40 @@
-from flask import Flask
-from pinterest_routes import pinterest_routes
+from flask import Flask, request, jsonify, render_template
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-# Register blueprint for Pinterest routes
-app.register_blueprint(pinterest_routes)
+@app.route('/pinterest_scraper')
+def pinterest_scraper():
+    # Get the search query from the request parameters
+    search_query = request.args.get('query')
+
+    if not search_query:
+        return jsonify({'error': 'Please provide a search query'})
+
+    # URL of the Pinterest search page
+    url = f'https://www.pinterest.com/search/pins/?q={search_query}'
+
+    # Send a GET request to the URL
+    response = requests.get(url)
+
+    # Parse the HTML content
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Find the elements containing the data you want to scrape
+    pins = soup.find_all('div', class_='Pin')
+
+    # Extract the information you need from the elements
+    scraped_data = []
+    for pin in pins:
+        pin_data = {
+            'title': pin.find('h3', class_='Pin__title').text,
+            'image_url': pin.find('img', class_='PinImage').get('src')
+        }
+        scraped_data.append(pin_data)
+
+    # Render template with scraped data
+    return render_template('pinterest.html', data=scraped_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
