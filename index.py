0a4d245d7fd83fba1c82069680ed3b6d97725c3e
Akashi433 <tohkarinka464@gmail.com>
2024-05-09 08:32:17 +0700

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template
 from scraper import scrape_images
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
